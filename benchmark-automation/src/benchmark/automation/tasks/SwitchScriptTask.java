afd9f7aa14c241d1973c1ed59cb97ceb3e833fc5
tomwang2011 <tong.wang@liferay.com>
2016-03-16 16:16:39 -0700

    Added script switcher target

--- benchmark-automation/src/benchmark/automation/tasks/SwitchScriptTask.java
+++ benchmark-automation/src/benchmark/automation/tasks/SwitchScriptTask.java
@@ -11,9 +11,10 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.tools.ant.Task;
 
 /**
@@ -23,11 +24,49 @@ public class SwitchScriptTask extends Task {
     public void execute() {
 		try {
 			replaceEnv(_environment, _filePath);
+
+			appendScript(_filePath, _script);
 		}
 		catch (IOException ex) {
-			Logger.getLogger(SwitchScriptTask.class.getName()).log(Level.SEVERE, null, ex);
 		}
     }
+
+	public void appendScript(Path filePath, String script) throws IOException {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(
+			"database.sample.data.properties=database/sample-data/"
+				+ "sample-sql-builder-");
+		sb.append(script);
+		sb.append(".properties\n");
+		sb.append("database.name=lportal_");
+		sb.append(script);
+		sb.append('\n');
+		sb.append("grinder.script=script/");
+		Map<String, String> scriptMap = initMap();
+		sb.append(scriptMap.get(script));
+		sb.append(".py");
+		sb.append('\n');
+		sb.append("sample.archive.dir=archive/");
+		sb.append(script);
+
+		Files.write(
+			filePath, Arrays.asList(sb.toString()), StandardOpenOption.APPEND);
+	}
+
+	public Map<String, String> initMap() {
+		Map<String, String> scriptMap = new HashMap<>();
+
+		scriptMap.put("asset", "assetpublisher");
+		scriptMap.put("blog", "blog");
+		scriptMap.put("content", "content");
+		scriptMap.put("login", "login");
+		scriptMap.put("mb", "messageboard");
+		scriptMap.put("wiki", "wiki");
+		scriptMap.put("dl", "documentlibrary");
+
+		return scriptMap;
+	}
 
 	public void replaceEnv(String env, Path filePath) throws IOException {
 		String content = new String(Files.readAllBytes(filePath));
