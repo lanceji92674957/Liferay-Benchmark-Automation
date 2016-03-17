1e204060f9168e274050f01f7e01b01ee23f576c
tomwang2011 <tong.wang@liferay.com>
2016-03-17 15:40:14 -0700

    Add more modifiable properties

--- src/benchmark/automation/tasks/SwitchScriptTask.java
+++ src/benchmark/automation/tasks/SwitchScriptTask.java
@@ -6,6 +6,8 @@
 
 package benchmark.automation.tasks;
 
+import benchmark.automation.util.PropertiesUtil;
+import benchmark.automation.util.StringUtil;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -15,6 +17,7 @@ import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import org.apache.tools.ant.Task;
 
 /**
@@ -23,7 +26,7 @@ import org.apache.tools.ant.Task;
 public class SwitchScriptTask extends Task {
     public void execute() {
 		try {
-			Map<String, String> scriptMap = initMap();
+			Map<String, Settings> scriptMap = initMap();
 
 			verifyInputs(scriptMap);
 
@@ -38,7 +41,7 @@ public class SwitchScriptTask extends Task {
     }
 
 	public void appendScript(
-			Map<String, String> scriptMap, Path filePath, String script)
+			Map<String, Settings> scriptMap, Path filePath, String script)
 		throws IOException {
 
 		StringBuilder sb = new StringBuilder();
@@ -52,26 +55,77 @@ public class SwitchScriptTask extends Task {
 		sb.append(script);
 		sb.append('\n');
 		sb.append("grinder.script=script/");
-		sb.append(scriptMap.get(script));
+
+		Settings scriptSettings = scriptMap.get(script);
+
+		sb.append(scriptSettings.getFullName());
 		sb.append(".py");
 		sb.append('\n');
 		sb.append("sample.archive.dir=archive/");
 		sb.append(script);
+		sb.append('\n');
+		sb.append("agent.new.thread.model.init.permits=");
+		sb.append(scriptSettings.getInitPermits());
+		sb.append('\n');
+		sb.append("sample.heap.enabled=");
+		sb.append(scriptSettings.getSampleHeap());
+		sb.append('\n');
+		sb.append("sample.heap.liveonly=");
+		sb.append(scriptSettings.getSampleHeap());
+
+		String sbString = sb.toString();
+		byte[] sbBytes = sbString.getBytes();
 
 		Files.write(
-			filePath, Arrays.asList(sb.toString()), StandardOpenOption.APPEND);
+			filePath, sbBytes, StandardOpenOption.APPEND);
 	}
 
-	public Map<String, String> initMap() {
-		Map<String, String> scriptMap = new HashMap<>();
+	public Map<String, Settings> initMap() throws IOException {
+		Map<String, Settings> scriptMap = new HashMap<>();
 
-		scriptMap.put("asset", "assetpublisher");
-		scriptMap.put("blog", "blog");
-		scriptMap.put("content", "content");
-		scriptMap.put("login", "login");
-		scriptMap.put("mb", "messageboard");
-		scriptMap.put("wiki", "wiki");
-		scriptMap.put("dl", "documentlibrary");
+		Properties scriptProperties = PropertiesUtil.loadProperties(
+			Paths.get("script.properties"));
+
+		Settings assetSettings =
+			new Settings(
+				"assetpublisher", scriptProperties.getProperty("asset"),
+				"false");
+
+		scriptMap.put("asset", assetSettings);
+
+		Settings blogSettings =
+			new Settings("blog", scriptProperties.getProperty("blog"), "false");
+
+		scriptMap.put("blog", blogSettings);
+
+		Settings contentSettings =
+			new Settings(
+				"content", scriptProperties.getProperty("content"), "false");
+
+		scriptMap.put("content", contentSettings);
+
+		Settings loginSettings =
+			new Settings(
+				"login", scriptProperties.getProperty("login"), "true");
+
+		scriptMap.put("login", loginSettings);
+
+		Settings mbSettings =
+			new Settings(
+				"messageboard", scriptProperties.getProperty("mb"), "false");
+
+		scriptMap.put("mb", mbSettings);
+
+		Settings wikiSettings =
+			new Settings("wiki", scriptProperties.getProperty("wiki"), "false");
+
+		scriptMap.put("wiki", wikiSettings);
+
+		Settings dlSettings =
+			new Settings(
+				"documentlibrary", scriptProperties.getProperty("dl"), "false");
+
+		scriptMap.put("dl", dlSettings);
 
 		return scriptMap;
 	}
@@ -79,7 +133,7 @@ public class SwitchScriptTask extends Task {
 	public void replaceEnv(String env, Path filePath) throws IOException {
 		String content = new String(Files.readAllBytes(filePath));
 
-		content = content.replace("qa1", _environment);
+		content = StringUtil.replace(content, "qa1", _environment);
 
 		Files.write(
 			filePath, Arrays.asList(content), Charset.defaultCharset());
@@ -97,7 +151,7 @@ public class SwitchScriptTask extends Task {
         _script = script;
     }
 
-	public void verifyInputs(Map<String, String> scriptMap) throws IOException {
+	public void verifyInputs(Map<String, Settings> scriptMap) throws IOException {
 		if (!scriptMap.containsKey(_script)) {
 			throw new IOException(_script + " is not a valid script!");
 		}
@@ -110,4 +164,30 @@ public class SwitchScriptTask extends Task {
 	private String _environment;
 	private Path _filePath;
 	private String _script;
+
+	private class Settings {
+		public Settings(
+			String fullName, String initPermits, String sampleHeap) {
+
+			_fullName = fullName;
+			_initPermits = initPermits;
+			_sampleHeap = sampleHeap;
+		}
+
+		public String getFullName() {
+			return _fullName;
+		}
+
+		public String getInitPermits() {
+			return _initPermits;
+		}
+
+		public String getSampleHeap() {
+			return _sampleHeap;
+		}
+
+		private String _fullName;
+		private String _initPermits;
+		private String _sampleHeap;
+	}
 }
