4c25d2706b527002e79f2cd7c2c0e259ce61a9aa
tomwang2011 <tong.wang@liferay.com>
2016-12-08 15:33:44 -0800

    Test crontab properties

--- baseline.sh
+++ baseline.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
+ANT_OPTS="-server -Xverify:none -XX:NewSize=128m -XX:MaxNewSize=256m -Xms1024m -Xmx1536m -XX:+UseParallelOldGC -XX:SurvivorRatio=65536 -XX:TargetSurvivorRatio=0 -XX:MaxTenuringThreshold=0 -XX:+DisableExplicitGC -Xss2m -XX:PermSize=100m -XX:MaxPermSize=256m"
+JAVA_HOME=/opt/jvms/default_jdk
+ANT_HOME=/opt/ant
+MAVEN_HOME=/opt/maven
+PATH=$JAVA_HOME/bin:$ANT_HOME/bin:$MAVEN_HOME/bin:/home/zsyxc/localwork/bin:$GIT_CORE:$ASPECTJ_HOME/bin:$PATH
+export JAVA_HOME ANT_HOME MAVEN_HOME ANT_OPTS PATH
+
 cd /home/trunks/git/liferay-portal
 
 git checkout master
