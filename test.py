0a6ca290beeed54a7e5f5216ea067c5694f74b81
tomwang2011 <tong.wang@liferay.com>
2017-02-02 16:01:37 -0800

    Python script for test

---
+++ test.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import shutil
+import subprocess
+import time
+
+def runBashCommand(bashCommand):
+	process = subprocess.Popen(bashCommand.split())
+	time.sleep(5)
+
+def copyProperties(filePath):
+	shutil.copyfile(filePath, benchmarkDir + "/benchmark-ext.properties")
+
+def runTest(testCase):
+	copyProperties("benchmark-exts/" + testCase + "/benchmark-ext.properties")
+	os.chdir(benchmarkDir)
+	runBashCommand("ant stop reload-warmup-database all-portal start-visualvm all-grinder all-sample stop -Dskip.build.portal=true")
+	os.chdir(currentDir)
+
+currentDir = os.getcwd()
+benchmarkDir = "/liferaysc/liferay-benchmark-ee"
+
+for arg in sys.argv[1:]:
+	runTest(arg)
