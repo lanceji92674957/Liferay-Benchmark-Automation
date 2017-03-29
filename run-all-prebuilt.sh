831bb38dd62922d38a99807a1208270a322b111c
tomwang2011 <tong.wang@liferay.com>
2017-03-29 13:48:19 -0700

    Add scripts to run warmup then test

---
+++ run-all-prebuilt.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+ANT_OPTS="-server -Xverify:none -XX:NewSize=128m -XX:MaxNewSize=256m -Xms1024m -Xmx1536m -XX:+UseParallelOldGC -XX:SurvivorRatio=65536 -XX:TargetSurvivorRatio=0 -XX:MaxTenuringThreshold=0 -XX:+DisableExplicitGC -Xss2m -XX:PermSize=100m -XX:MaxPermSize=256m"
+JAVA_HOME=/opt/jvms/default_jdk
+ANT_HOME=/opt/ant
+MAVEN_HOME=/opt/maven
+PATH=$JAVA_HOME/bin:$ANT_HOME/bin:$MAVEN_HOME/bin:/home/zsyxc/localwork/bin:$GIT_CORE:$ASPECTJ_HOME/bin:$PATH
+export JAVA_HOME ANT_HOME MAVEN_HOME ANT_OPTS PATH
+
+cd /home/trunks/git/liferay-benchmark-ee
+
+echo *************Starting all-database*********
+
+ant all-database -Dskip.build.portal=true
+
+echo *************Starting baseline*************
+
+ant stop reload-warmup-database all-portal start-visualvm all-grinder all-sample stop -Dskip.build.portal=true
